86d80f231d10ba9f2361d9187400ca4e6af828e5
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2019-02-22 11:53:27 -0600

    Added gravity and movement

---
+++ Assets/Scripts/GravityScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour
+{ 
+
+    float VerticalInput;
+    float HorizontalInput;
+    Movement GravityMovement;
+
+    GameObject[] ApplesList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ApplesList = GameObject.FindGameObjectsWithTag("Apple");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplesList = GameObject.FindGameObjectsWithTag("Apple");
+
+        VerticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+
+        // If VerticalInput is positive, means gravity is going up.
+        if (VerticalInput > 0)
+        {
+            foreach (GameObject Apple in ApplesList)
+            {
+                GravityMovement = Apple.GetComponent<Movement>();
+                if (GravityMovement.Gravity < 0)
+                    GravityMovement.Gravity = -GravityMovement.Gravity;
+                Apple.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                GravityMovement.VerticalDirection = true;
+            }
+        }
+        // If VerticalInput is negative, means gravity is going down.
+        if (VerticalInput < 0)
+        {
+            foreach (GameObject Apple in ApplesList)
+            {
+                GravityMovement = Apple.GetComponent<Movement>();
+                if (GravityMovement.Gravity > 0)
+                    GravityMovement.Gravity = -GravityMovement.Gravity;
+                Apple.transform.rotation = Quaternion.Euler(0, 0, 180);
+                GravityMovement.VerticalDirection = true;
+            }
+        }
+
+        // If Horizontal is postive, means gravity is going right.
+        if (HorizontalInput > 0)
+        {
+            foreach (GameObject Apple in ApplesList)
+            {
+                GravityMovement = Apple.GetComponent<Movement>();
+                if (GravityMovement.Gravity < 0)
+                    GravityMovement.Gravity = -GravityMovement.Gravity;
+                Apple.transform.rotation = Quaternion.Euler(0, 0, -90);
+                GravityMovement.VerticalDirection = false;
+            }
+        }
+        // If Horizontal is negative, means gravity is going left.
+        if (HorizontalInput < 0)
+        {
+            foreach (GameObject Apple in ApplesList)
+            {
+                GravityMovement = Apple.GetComponent<Movement>();
+                if (GravityMovement.Gravity > 0)
+                    GravityMovement.Gravity = -GravityMovement.Gravity;
+                Apple.transform.rotation = Quaternion.Euler(0, 0, 90);
+                GravityMovement.VerticalDirection = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Gravity = 9.8f;
+    public bool VerticalDirection = true;
+
+    Rigidbody2D rbApple;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbApple = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VerticalDirection)
+        {
+            rbApple.velocity = new Vector2(0, Gravity);
+        }
+        if (!VerticalDirection)
+        {
+            rbApple.velocity = new Vector2(Gravity, 0);
+        }
+    }
+}
