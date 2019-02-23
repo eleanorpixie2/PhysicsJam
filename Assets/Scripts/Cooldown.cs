b75ad4499b558156e16e760507a194c674615602
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-02-23 12:20:22 -0600

    Updated cooldown script

--- Assets/Scripts/Cooldown.cs
+++ Assets/Scripts/Cooldown.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class Cooldown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	float VerticalInput;
+    float HorizontalInput;
+    Movement GravityMovement;
+
+    GameObject[] ApplesList;
+
+    public float cooldownTime = 2.0f;
+
+	private float nextFireTime = 1.0f;
+
+	private void Update()
+	{
+		ApplesList = GameObject.FindGameObjectsWithTag("Apple");
+
+        VerticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+
+        // If VerticalInput is positive, means gravity is going up.
+        if(Time.Time > nextFireTime)
+		{
+			if (VerticalInput > 0)
+			{
+				foreach (GameObject Apple in ApplesList)
+				{
+					GravityMovement = Apple.GetComponent<Movement>();
+					if (GravityMovement.Gravity < 0)
+						GravityMovement.Gravity = -GravityMovement.Gravity;
+					Apple.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+					nextFireTime = Time.time + cooldownTime;
+
+					GravityMovement.VerticalDirection = true;
+				}
+			}
+			// If VerticalInput is negative, means gravity is going down.
+			if (VerticalInput < 0)
+			{
+				foreach (GameObject Apple in ApplesList)
+				{
+					GravityMovement = Apple.GetComponent<Movement>();
+					if (GravityMovement.Gravity > 0)
+						GravityMovement.Gravity = -GravityMovement.Gravity;
+					Apple.transform.rotation = Quaternion.Euler(0, 0, 180);
+
+					nextFireTime = Time.time + cooldownTime;
+
+					GravityMovement.VerticalDirection = true;
+				}
+			}
+
+			// If Horizontal is postive, means gravity is going right.
+			if (HorizontalInput > 0)
+			{
+				foreach (GameObject Apple in ApplesList)
+				{
+					GravityMovement = Apple.GetComponent<Movement>();
+					if (GravityMovement.Gravity < 0)
+						GravityMovement.Gravity = -GravityMovement.Gravity;
+					Apple.transform.rotation = Quaternion.Euler(0, 0, -90);
+
+					nextFireTime = Time.time + cooldownTime;
+
+					GravityMovement.VerticalDirection = false;
+				}
+			}
+			// If Horizontal is negative, means gravity is going left.
+			if (HorizontalInput < 0)
+			{
+				foreach (GameObject Apple in ApplesList)
+				{
+					GravityMovement = Apple.GetComponent<Movement>();
+					if (GravityMovement.Gravity > 0)
+						GravityMovement.Gravity = -GravityMovement.Gravity;
+					Apple.transform.rotation = Quaternion.Euler(0, 0, 90);
+
+					nextFireTime = Time.time + cooldownTime;
+
+					GravityMovement.VerticalDirection = false;
+				}
+			}
+		}
+	}
 }
